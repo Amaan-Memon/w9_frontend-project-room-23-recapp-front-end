9c960881d2e4f1217d15836c99a1cb0babc17ac0
Amaan Memon <thatboyycaesar@Amaans-MBP.broadband>
2022-06-21 11:41:57 +0100

    Navbar returns components

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -4,3 +4,27 @@
 //2. Search Bar
 //3. Weeks drop down menu
 //4. Glossary button
+
+import React from 'react';
+import SearchBar from '../SearchBar/SearchBar'
+import SearchButton from '../SearchButton/SearchButton'
+import WeeksDropDown from '../WeeksDropDown/WeeksDropDown'
+import GlossaryButton from '../GlossaryButton/GlossaryButton'
+
+function NavBar(){
+    return(
+        <div className="navbar"> 
+            <img className = "logo" src="https://www.freepnglogos.com/uploads"/>
+            <div className="search">
+                <SearchBar/>
+                <SearchButton/>
+            </div>
+            <div className="navbar--menus">
+                <WeeksDropDown className="weeks-drop-down"/>
+                <GlossaryButton className="glossary"/>
+            </div>
+        </div>        
+    );
+}
+
+export default NavBar;
