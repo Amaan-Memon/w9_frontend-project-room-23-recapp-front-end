26e8f9f4b3951bfbe01220f7196c284d5d43339c
Amaan Memon <thatboyycaesar@Amaans-MBP.broadband>
2022-06-21 10:25:13 +0100

    Layout plan finished

--- src/Landing.js
+++ src/Landing.js
@@ -1,3 +1,14 @@
 //LANDING PAGE 
 //This is the landing page for the application and will have the following components: 
 //1. NavBar
+// - Logo
+// - Search Bar
+// - Weeks drop down menu
+// - Glossary button
+//2. Random Quotes Generator
+// - Random quotes will generate from the quotes API
+//3. Mission Statement
+// - A welcome message and mission Statement will be displayed on the landing page
+//4. Footer
+// - Copyright
+// - Links to other pages
