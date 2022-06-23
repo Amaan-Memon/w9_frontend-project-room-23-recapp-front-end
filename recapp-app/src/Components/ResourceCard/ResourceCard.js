61a4173d689017e2bad0a46479640511da817c94
Amaan Memon <thatboyycaesar@Amaans-MBP.broadband>
2022-06-23 14:28:59 +0100

    search results page error fetching

--- recapp-app/src/Components/ResourceCard/ResourceCard.js
+++ recapp-app/src/Components/ResourceCard/ResourceCard.js
@@ -4,7 +4,7 @@ import "./Card.css"
 function ResourceCard({resourcesLink, resourcesTopic, resourcesValue, resourcesWeek, resourcesImage}){
     return (
         <div className="content--card">
-            <img className="card--image" href={resourcesImage} alt="Resource"/>
+            <img className="card--image" src={resourcesImage} alt="Resource" style={{width:"15%"}}/>
             <a href={resourcesLink}><h4>{resourcesTopic}</h4></a>
             <h3>Week {resourcesWeek}</h3>
             <h2>{resourcesValue} Likes</h2>
