707731fceadf8e2f45811daeb3918adae9bdcd4e
mateuszsoszynski <soszynski94@googlemail.com>
2022-06-23 11:28:09 +0100

    week content

--- recapp-app/src/Components/GlossaryCard/GlossaryCard.js
+++ recapp-app/src/Components/GlossaryCard/GlossaryCard.js
@@ -5,16 +5,9 @@ function GlossaryCard({ glossaryWord, glossaryDefinition }){
 
 
     return(
-<<<<<<< HEAD
-        <div className="glossary--card">
-            {/* <h3>Word</h3>
-            <h4>Definition</h4> */}
-            
-=======
         <div className="glossary--card" id="Glossary">
             <h3>{glossaryWord}</h3>
             <h4>{glossaryDefinition}</h4>
->>>>>>> 3cb2cd913b821212f6e6af55c570c63df2d9cbfe
         </div>
     )
 }
