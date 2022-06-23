7bc18094bb4673b1bcc8355ad7615400553b21e9
Amaan Memon <thatboyycaesar@Amaans-MBP.broadband>
2022-06-23 12:32:20 +0100

    weeks page works

--- recapp-app/src/Components/ResourceCard/ResourceCard.js
+++ recapp-app/src/Components/ResourceCard/ResourceCard.js
@@ -4,7 +4,7 @@ import "./Card.css"
 function ResourceCard({resourcesLink, resourcesTopic, resourcesValue, resourcesWeek, resourcesImage}){
     return (
         <div className="content--card">
-            <img className="card--image" href="" alt="Resource"/>
+            <img className="card--image" href={resourcesImage} alt="Resource"/>
             <a href={resourcesLink}><h4>{resourcesTopic}</h4></a>
             <h3>Week {resourcesWeek}</h3>
             <h2>{resourcesValue} Likes</h2>
