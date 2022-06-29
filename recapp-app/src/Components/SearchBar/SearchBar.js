5f33b68237be9f806b3fa89bb52f9e28ad4cffd1
NoahAldhous <naldhous@hotmail.co.uk>
2022-06-29 10:33:15 +0100

    removed search bar comments on top.

--- recapp-app/src/Components/SearchBar/SearchBar.js
+++ recapp-app/src/Components/SearchBar/SearchBar.js
@@ -1,11 +1,3 @@
-//Search Bar
-// - Created a function called SearchBar which takes in the setUserValue 
-//   function as a parameter 
-// - useEffect is used to fetch data async from the backend, format 
-//   it as a JSON and then setData function sets state to the data's 
-//   payload
-// - Returns an input and a search button which redirects through  
-//   useNavigate to search result page 
 
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
